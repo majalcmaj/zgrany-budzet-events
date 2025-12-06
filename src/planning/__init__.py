f5a00b14bf0ac432302211ea9be7c2891d2eb41e
WojSkwie <wojtek.skwierawski@gmail.com>
2025-12-06 22:03:22 +0100

    Added minister view

--- src/planning/__init__.py
+++ src/planning/__init__.py
@@ -56,6 +56,31 @@ def chief_dashboard():
 
     return render_template('chief_dashboard.html', state=planning_state, offices_status=offices_status, total_all_needs=total_all_needs)
 
+@planning_bp.route('/minister_dashboard', methods=['GET', 'POST'])
+@auth_required
+def minister_dashboard():
+    from src.expenses import EXPENSES, EXPENSES_CLOSED
+    
+    offices_status = []
+    total_all_needs = 0
+    for office in ['office1', 'office2']:
+        expenses = EXPENSES.get(office, [])
+        total_needs = sum(e['financial_needs'] for e in expenses if e['financial_needs'] is not None)
+        task_count = len(expenses)
+        is_submitted = EXPENSES_CLOSED.get(office, False)
+        
+        total_all_needs += total_needs
+
+        offices_status.append({
+            'name': office,
+            'status': 'Submitted' if is_submitted else 'Open',
+            'total_needs': total_needs,
+            'task_count': task_count,
+            'expenses': expenses
+        })
+
+    return render_template('minister_dashboard.html', state=planning_state, offices_status=offices_status, total_all_needs=total_all_needs)
+
 @planning_bp.route('/')
 def index():
     return render_template('role_selection.html')
@@ -69,4 +94,6 @@ def set_role():
             return redirect(url_for('planning.chief_dashboard'))
         elif role in ['office1', 'office2']:
             return redirect(url_for('expenses.list_expenses'))
+        elif role == 'minister':
+            return redirect(url_for('planning.minister_dashboard'))
     return redirect(url_for('planning.index'))
