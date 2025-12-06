174409568e9809a44deb77c045d2202ac5ab5db0
Michał Ciesielski <majalcmaj@gmail.com>
2025-12-06 16:31:45 +0100

    #4 - simple expenses list

--- src/expenses/__init__.py
+++ src/expenses/__init__.py
@@ -1,7 +1,21 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
 
 expenses_bp = Blueprint('expenses', __name__)
 
-@expenses_bp.route('/add')
+EXPENSES = []
+
+@expenses_bp.route('/add', methods=['GET', 'POST'])
 def add_expense():
+    if request.method == 'POST':
+        expense = {
+            'chapter': request.form.get('chapter'),
+            'task_name': request.form.get('task_name'),
+            'financial_needs': request.form.get('financial_needs')
+        }
+        EXPENSES.append(expense)
+        return redirect(url_for('expenses.list_expenses'))
     return render_template('add_expense.html')
+
+@expenses_bp.route('/')
+def list_expenses():
+    return render_template('expenses_list.html', expenses=EXPENSES)
