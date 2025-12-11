668a13fddc9de59da73b95025d82f3645250d0d7
Michał Ciesielski <majalcmaj@gmail.com>
2025-12-11 23:41:18 +0100

    Implement a basic event store

--- flaskr/events/event_store.py
+++ flaskr/events/event_store.py
@@ -1,2 +1,45 @@
+from typing import Callable, Any
+import inspect
+
+
 class EventStore:
-    pass
+    def __init__(self):
+        # Dictionary mapping event types to lists of handler functions
+        self._subscribers: dict[type, list[Callable[[Any], None]]] = {}
+
+    def add_subscriber(self, handler: Callable[[Any], None]) -> None:
+        """
+        Add a subscriber handler function.
+        The handler's first parameter type annotation determines which event type it handles.
+        """
+        # Get the handler's signature
+        sig = inspect.signature(handler)
+        params = list(sig.parameters.values())
+
+        if not params:
+            raise ValueError("Handler must accept at least one parameter (the event)")
+
+        # Get the type annotation of the first parameter
+        event_type = params[0].annotation
+
+        if event_type == inspect.Parameter.empty:
+            raise ValueError("Handler's first parameter must have a type annotation")
+
+        # Add the handler to the list of subscribers for this event type
+        if event_type not in self._subscribers:
+            self._subscribers[event_type] = []
+
+        self._subscribers[event_type].append(handler)
+
+    def emit(self, event: Any) -> None:
+        """
+        Emit an event to all registered handlers for that event type.
+        """
+        event_type = type(event)
+
+        # Get all handlers for this event type
+        handlers = self._subscribers.get(event_type, [])
+
+        # Call each handler with the event
+        for handler in handlers:
+            handler(event)
--- flaskr/events/event_store_test.py
+++ flaskr/events/event_store_test.py
@@ -1,32 +1,35 @@
 from .event_store import EventStore
 
 
-class TestEvent:
+class MockEvent:
     def __init__(self, id: int):
         self.id = id
 
+    def __eq__(self, other):
+        return isinstance(other, MockEvent) and self.id == other.id
 
-class TestEvent2:
+
+class MockEvent2(MockEvent):
     def __init__(self, id: int):
-        self.id = id
+        super().__init__(id)
 
 
-class TestEvent3:
+class MockEvent3(MockEvent):
     def __init__(self, id: int):
-        self.id = id
+        super().__init__(id)
 
 
 class Subscriber:
     def __init__(self):
         self.handled_events = []
 
-    def handle_test_event(self, event: TestEvent):
+    def handle_test_event(self, event: MockEvent):
         self.handled_events.append(event)
 
-    def handle_test_event2(self, event: TestEvent2):
+    def handle_test_event2(self, event: MockEvent2):
         self.handled_events.append(event)
 
-    def handle_test_event3(self, event: TestEvent3):
+    def handle_test_event3(self, event: MockEvent3):
         self.handled_events.append(event)
 
 
@@ -37,10 +40,10 @@ def test_single_event_multiple_subscribers():
     event_store.add_subscriber(subscriber1.handle_test_event)
     event_store.add_subscriber(subscriber2.handle_test_event)
 
-    event_store.emit(TestEvent(1))
+    event_store.emit(MockEvent(1))
 
-    assert subscriber1.handled_events == [TestEvent(1)]
-    assert subscriber2.handled_events == [TestEvent(1)]
+    assert subscriber1.handled_events == [MockEvent(1)]
+    assert subscriber2.handled_events == [MockEvent(1)]
 
 
 def test_many_events():
@@ -52,12 +55,12 @@ def test_many_events():
     event_store.add_subscriber(subscriber2.handle_test_event)
     event_store.add_subscriber(subscriber2.handle_test_event2)
 
-    event_store.emit(TestEvent(1))
-    event_store.emit(TestEvent2(1))
-    event_store.emit(TestEvent(2))
+    event_store.emit(MockEvent(1))
+    event_store.emit(MockEvent2(1))
+    event_store.emit(MockEvent(2))
 
-    assert subscriber1.handled_events == [TestEvent(1), TestEvent2(1), TestEvent(2)]
-    assert subscriber2.handled_events == [TestEvent(1), TestEvent2(1), TestEvent(2)]
+    assert subscriber1.handled_events == [MockEvent(1), MockEvent2(1), MockEvent(2)]
+    assert subscriber2.handled_events == [MockEvent(1), MockEvent2(1), MockEvent(2)]
 
 
 def test_different_events():
@@ -69,9 +72,23 @@ def test_different_events():
     event_store.add_subscriber(subscriber2.handle_test_event2)
     event_store.add_subscriber(subscriber2.handle_test_event3)
 
-    event_store.emit(TestEvent(1))
-    event_store.emit(TestEvent2(2))
-    event_store.emit(TestEvent3(3))
+    event_store.emit(MockEvent(1))
+    event_store.emit(MockEvent2(2))
+    event_store.emit(MockEvent3(3))
 
-    assert subscriber1.handled_events == [TestEvent(1), TestEvent2(2)]
-    assert subscriber2.handled_events == [TestEvent2(2), TestEvent3(3)]
+    assert subscriber1.handled_events == [MockEvent(1), MockEvent2(2)]
+    assert subscriber2.handled_events == [MockEvent2(2), MockEvent3(3)]
+
+
+def test_non_class_method():
+    event_store = EventStore()
+    events_handled = []
+
+    def handle_test_event(event: MockEvent):
+        events_handled.append(event)
+
+    event_store.add_subscriber(handle_test_event)
+
+    event_store.emit(MockEvent(1))
+
+    assert events_handled == [MockEvent(1)]
